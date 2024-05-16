fb4ee7e7d291b1ec3dbdeaedbaccb7eda199645a
jula <julianowak077@gmail.com>
2024-05-17 00:22:24 +0200

    fix

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require("express");
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const testimonialsRoutes = require("./routes/testimonials.routes");
 const seatsRoutes = require("./routes/seats.routes");
 const concertsRoutes = require("./routes/concerts.routes");
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/api", testimonialsRoutes);
 app.use("/api", seatsRoutes);
 app.use("/api", concertsRoutes);
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).json({ message: "Not found..." });
 });
